881725ec893e0981d6bf340e0e28648101e24b6f
BSpoones <76079263+BSpoones@users.noreply.github.com>
2025-03-07 00:31:09 +0000

    misc fixes

--- model/lib/util/enums/ElementTypeEnum.py
+++ model/lib/util/enums/ElementTypeEnum.py
@@ -1,59 +1,61 @@
-from enum import Enum
+from enum import IntEnum
 from abc import ABC, abstractmethod
 from EnumMeta import _EnumMeta
 
 
-class ElementType(ABC, Enum, metaclass=_EnumMeta):
+class ElementType(ABC, IntEnum, metaclass=_EnumMeta):
     
     @staticmethod
     def from_ordinal(ordinal: int):
         """Returns the enum member corresponding to the given ordinal."""
+
+        if ordinal == 0:
+            return None
+
         members = list(ElementType)
         
-        if 0 <= ordinal < len(members):
+        if 1 <= ordinal < len(members):
             return members[ordinal]
         raise ValueError(f"Invalid ordinal {ordinal}")
     
     @staticmethod
     def max() -> int:
-        members = list(ElementType)
-        if (len(members) < 1):
+        members: list[ElementType] = list(ElementType)
+        if not members:
             return 0
         
-        return list(map(members, lambda x: x.ordinal())).max()
+        return max(map(lambda x: x.ordinal(), members))
         
     
     @staticmethod
     def min() -> int:
         members = list(ElementType)
-        if (len(members) < 1):
+        if not members:
             return 0
         
-        return list(map(members, lambda x: x.ordinal())).min()
-    
+        return min(map(lambda x: x.ordinal(), members))
+
+    @property
+    @abstractmethod
+    def raw_file(self) -> str:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def paraphrased_file(self) -> str:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def normalised_file(self) -> str:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def classified_file(self) -> str:
+        raise NotImplementedError
+
 
     def ordinal(self) -> int:
         """Returns the index (ordinal) of the enum member."""
         return list(self.__class__).index(self)
-        
-    @property
-    @abstractmethod
-    def raw_file() -> str:
-        raise NotImplementedError
-    
-    @property
-    @abstractmethod
-    def paraphrased_file() -> str:
-        raise NotImplementedError
-    
-    @property
-    @abstractmethod
-    def normalised_file() -> str:
-        raise NotImplementedError
-    
-    @property
-    @abstractmethod
-    def classified_file() -> str:
-        raise NotImplementedError
-    
-    
