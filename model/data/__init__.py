a63d324303fdc5b19d3da0d1500f3a4462d81b4e
BSpoones <76079263+BSpoones@users.noreply.github.com>
2025-03-22 00:45:49 +0000

    add: raw file locations

---
+++ model/data/__init__.py
@@ -0,0 +1,21 @@
+"""
+
+TODO -> Doc
+"""
+
+# Relative file names assume this is run in the model directory
+
+
+
+RAW_FILES = [
+    ".model/data/raw/power_imbalance.csv",
+    ".model/data/raw/downplaying.csv",
+    ".model/data/raw/invalidation.csv",
+    ".model/data/raw/emotional_leverage.csv",
+    ".model/data/raw/expectation.csv",
+    ".model/data/raw/memory_distortion.csv",
+    ".model/data/raw/emotional_connection.csv",
+    ".model/data/raw/blame.csv",
+    ".model/data/raw/denial.csv",
+    ".model/data/raw/comparative_justification.csv"
+]
