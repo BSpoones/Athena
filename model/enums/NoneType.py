0999e20725c9e9f4c683e61ff16dcd832621e08a
BSpoones <76079263+BSpoones@users.noreply.github.com>
2025-04-05 16:30:33 +0100

    add: none data

---
+++ model/enums/NoneType.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class NoneType(Enum):
+    WEATHER = 1
+    FOOD = 2
+    ROUTINES = 3
+    WORK_STUDY = 4
+    TECHNOLOGY = 5
+    ENTERTAINMENT = 6
+    FITNESS = 7
+    TRAVEL = 8
+    SHOPPING = 9
+    TRIVIAL = 10
+    QUESTIONS = 11
+    JOKES = 12
+    SELF_STATEMENTS = 13
+    OBSERVATIONS = 14
+    INSTRUCTIONS = 15
+    DIALOGUE = 16
+    LOGISTICS = 17
+    PLANNING = 18
+    TRAVEL_TIPS = 19
+    AMBIENT_DESCRIPTION = 20
+
